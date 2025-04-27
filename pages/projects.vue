3e69633c454b0f003f6e66a187dd34ef2a6b461c
Kemeow815 <kemiao@kmblog.icu>
2025-04-27 20:36:06 +0800

    feat: update copyright and info

--- pages/projects.vue
+++ pages/projects.vue
@@ -13,9 +13,9 @@
 
 <script setup>
 const description =
-  "I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved.";
+  "感谢各位大佬的开源项目，希望你能用上！";
 useSeoMeta({
-  title: "Projects | Kris Yan",
+  title: "Projects | Ke Miao",
   description,
 });
 
